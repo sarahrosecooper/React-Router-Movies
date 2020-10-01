65073a1095b8cc3684c92edab61cb3b0f504cd25
sarah rose <xsarahrosemeow@gmail.com>
2020-09-30 22:23:15 -0400

    mvp

--- client/src/Movies/SavedList.js
+++ client/src/Movies/SavedList.js
@@ -1,13 +1,22 @@
-import React from 'react';
+import React from "react";
+import { useHistory } from "react-router-dom";
 
 export default function SavedList(props) {
+  const history = useHistory();
+  console.log("This is history", history);
+
+  const routeToHome = () => {
+    history.push(`/`);
+  };
   return (
     <div className="saved-list">
       <h3>Saved Movies:</h3>
-      {props.list.map(movie => (
+      {props.list.map((movie) => (
         <span className="saved-movie">{movie.title}</span>
       ))}
-      <div className="home-button">Home</div>
+      <div className="home-button" onClick={() => routeToHome()}>
+        Home
+      </div>
     </div>
   );
 }
